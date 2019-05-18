06c5786327877b85ebed67931fa2f3b4b4fdf18d
Михаил Белошицкий <itanka9@MacBook-Pro-Mihail.local>
2019-05-18 11:03:56 +0500

    autosize, step && vscale props

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,34 +1,45 @@
 <template>
   <div class="demo">
     <progress-icon
-      :size="36"
-      :progress="progress1"
-    >
-      <i class="fas fa-signal" />
-    </progress-icon>
-    <progress-icon
-      :size="96"
+      class="demo-medium"
       :progress="progress4"
       direction="horizontal"
     >
       <i class="fas fa-battery-full" />
     </progress-icon>
-     <progress-icon
-      :size="24"
+    <progress-icon
+      class="demo-big"
       :progress="progress3"
     >
       <i class="fas fa-signature" />
     </progress-icon>
+    <progress-icon
+      class="demo-small"
+      direction="horizontal"
+      :steps="5"
+      :progress="progress1"
+    >
+      <i class="fas fa-signal" />
+    </progress-icon>
+    <br />
     <template v-for="i in [1,2,3,4,5]">
       <progress-icon
-        :key="i"
-        :size="34"
+        :key="i"  
         :progress="progress2"
+        class="demo-small"
         color="red"
       >
         <i class="fas fa-biohazard" />
       </progress-icon>
     </template>
+    <br />
+    <progress-icon
+      class="demo-big"
+      :color="`rgb(${colorVal(colorR)}, ${colorVal(colorG)}, ${colorVal(colorB)})`"
+      :progress="progress3"
+    >
+      <i class="fas fa-bomb" />
+    </progress-icon>
   </div>
 </template>
 
@@ -47,6 +58,10 @@ export default {
       progress2: 0,
       progress3: 0,
       progress4: 0,
+
+      colorR: 0,
+      colorG: 0,
+      colorB: 0,
     }
   },
 
@@ -55,24 +70,37 @@ export default {
     setInterval(() => { this.progress2 = (this.progress2 + 1) % 100; }, 4)
     setInterval(() => { this.progress3 = (this.progress3 + 1) % 100; }, 42)
     setInterval(() => { this.progress4 = (this.progress4 + 1) % 100; }, 100)
+
+
+
+    setInterval(() => { this.colorR = (this.colorR + 1) % 512; }, 10)
+    setInterval(() => { this.colorG = (this.colorG + 1) % 512; }, 20)
+    setInterval(() => { this.colorB = (this.colorB + 1) % 512; }, 30)
+  },
+
+  methods: {
+    colorVal (val) {
+      return val < 256 ? val : 512 - val;
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.demo-small, .demo-medium, .demo-big {
+  margin: 16px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.demo-small {
+  font-size: 48px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.demo-big {
+  font-size: 112px;
 }
-a {
-  color: #42b983;
+
+.demo-medium {
+  font-size: 72px;
 }
 </style>
--- src/components/ProgressIcon.vue
+++ src/components/ProgressIcon.vue
@@ -1,13 +1,10 @@
 <template>
-    <div class="progress-icon"
-         :style="{ height: size + 'px', width: size + 'px' }">
-        <div class="bg"
-             :style="{ 'font-size': size + 'px' }"
-        >
+    <div class="progress-icon" ref="root">
+        <div class="bg" ref="measureContainer">
             <slot />
         </div>
         <div class="fg"
-             :style="{ clip: clipRect, 'font-size': size + 'px', color: color }"
+             :style="{ clip: clipRect, color: color }"
         >
             <slot />
         </div>
@@ -19,17 +16,21 @@ export default {
     name: 'progress-icon',
 
     props: {
-        size: {
+        progress: {
             type: Number,
-            default: 24
+            default: 0
+        },
+        steps: {
+            type: Number,
+            default: 0
         },
         color: {
             type: String,
             default: 'lightgreen'
         },
-        progress: {
+        vscale: {
             type: Number,
-            default: 0
+            default: 1.15
         },
         direction: {
             type: String,
@@ -39,12 +40,52 @@ export default {
 
     computed: {
         clipRect () {
-            const size = this.size + 1
+            const measured = this.measureSlotContent(this.progress)
+            if (!measured) {
+                return 'inherit'
+            }
+            this.adjustRoot(measured)
+            const { height, width } = measured
+            const scaledHeight = height * this.vscale
+            const gap = (scaledHeight - height) / 2 
+
             const dir = this.direction
-            const progress = size * this.progress / 100
-            const top = dir === 'vertical' ? (size - progress) : 0
-            const right = dir === 'horizontal' ? progress : size
-            return `rect(${top}px,${right}px,${size}px,0px)`
+            let progress = this.progress
+            if (this.steps) {
+                const step = 100 / this.steps
+                progress = Math.ceil(progress / step) * step
+            }
+            const top = dir === 'vertical' ? (scaledHeight - scaledHeight * progress / 100) : 0
+            const right = dir === 'horizontal' ? width * progress / 100 : width
+            return `rect(${top - gap}px,${right}px,${scaledHeight}px,0px)`
+        }
+    },
+
+    methods: {
+        measureSlotContent () {
+            const container = this.$refs.measureContainer
+            const slotElt = container && container.firstChild
+            if (!slotElt) {
+                return
+            }
+            return {
+                height: slotElt.clientHeight,
+                width: slotElt.clientWidth
+            }
+        },
+
+        adjustRoot (measured) {
+            const root = this.$refs.root
+            if (!root || !measured) {
+                return
+            }
+            const { height, width } = measured
+            if (root.clientHeight !== height) {
+                root.style.height = height + 'px'
+            }
+            if (root.clientWidth !== width) {
+                root.style.width = width + 'px'
+            }
         }
     }
 }
@@ -54,9 +95,10 @@ export default {
 <style scoped>
     .progress-icon {
         position: relative;
+        display: inline-block;
     }
 
-    .progress-icon .bg, .progress-icon .fg {
+    .progress-icon .fg, .progress-icon .bg {
         position: absolute;
         top: 0;
         left: 0;
